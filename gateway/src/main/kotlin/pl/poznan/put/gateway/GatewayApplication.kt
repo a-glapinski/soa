bb20362d86062d6c1a1b268daa55e68bd1a26d48
Adrian Glapiński <adrian.glapinski@rsqtechnologies.com>
2022-02-03 20:49:59 +0100

    Add dependencies

--- gateway/src/main/kotlin/pl/poznan/put/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/pl/poznan/put/gateway/GatewayApplication.kt
@@ -1,11 +1,16 @@
 package pl.poznan.put.gateway
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = Info(title = "SOA API", version = "1.0")
+)
 class GatewayApplication
 
 fun main(args: Array<String>) {
-	runApplication<GatewayApplication>(*args)
+    runApplication<GatewayApplication>(*args)
 }
